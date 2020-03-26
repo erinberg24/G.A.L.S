14987c565c7e8d745ae8e9fdffcc0a41ca6b8fd0
Sophie Appicelli <appicels@bc.edu>
2020-03-25 22:37:09 -0400

    cleaned

--- Delivery 3/testWFRONTENDATTEMPT/django-react/leads/models.py
+++
@@ -1,9 +0,0 @@
-# Create your models here.
-from django.db import models
-
-class Lead(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    eagleid = models.CharField(max_length=8)
-    password = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
